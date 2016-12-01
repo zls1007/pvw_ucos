f87510a3371de0d61a0a7df3b234b5d6bbdafdac
linshan <zls1007@163.com>
2016-12-02 00:03:39 +0800

    add adc filter function

--- TASK/src/adcDeal_task.c
+++ TASK/src/adcDeal_task.c
@@ -43,11 +43,66 @@ void adcDeal_task_create(void)
 	OS_CRITICAL_EXIT();		//退出临界区	
 }
 
+//均值滤波部分
+void filter(uint16_t ADC_ConvertedValue[][2])
+{
+	float sum = 0;
+	float temp;
+	float deep, vol;
+	float cur = 1.2;
+	float ADC_Filter[2];
+	SysState_Type mState;
+	static float deep_filter[5];  //移动平均滤波
+	static u8 index = 0;
+	
+	u8 count, i;
+	for(i=0; i<2; i++)
+	{
+		for(count=0; count<140; count ++)
+		{									   
+			temp = ADC_ConvertedValue[count][i] * 3.3/0x0fff;
+			sum += temp;
+		}
+		ADC_Filter[i] = sum/140;
+		sum = 0;
+	}
+
+	deep = ADC_Filter[1] * 1000/165;	//????? mA
+	deep = (deep - 4)*10;	 			//?????  16mA = 1.6Mpa   1Mpa = 100m
+	deep = (deep)*100/16;	 			//?????  16mA = 1Mpa   1Mpa = 100m
+
+	vol =  ADC_Filter[0] * 4;			// ??4
+
+
+	//移动平均滤波
+	deep_filter[index++] = deep;
+	if(index == 5) index = 0;
+	sum = 0;
+	for(i=0; i<5; i++)
+	{
+		sum += deep_filter[i];	
+	}
+	deep = sum/5;
+
+	//更新传感器参数
+	SetDeepData(&deep);
+	GetBatteryData(&vol, &cur);
+	
+	//检测是否欠压
+	if(vol < 10.7)
+	{
+		mState = MODE_ALARM_VOL;
+		SetSysState(&mState);
+	}
+		
+}
+
+
 void adcDeal_task(void *p_arg)
 {
 	OS_ERR err;
 
-	CPU_INT08U *delta;   	//存储块的指针
+	uint16_t *delta;   	//存储块的指针
 	OS_MSG_SIZE size;			//存储块的长度
 	
 //CPU_TS ts;  ts = OSTimeGet(&err);
@@ -57,7 +112,9 @@ void adcDeal_task(void *p_arg)
 	while(1)
 	{
 		//等待任务消息
-		delta = (CPU_INT08U* )OSTaskQPend(0, OS_OPT_PEND_BLOCKING, &size, 0, &err);
+		delta = (uint16_t* )OSTaskQPend(0, OS_OPT_PEND_BLOCKING, &size, 0, &err);
+		
+		filter((uint16_t(*)[2])delta);
 
 		
 		//把存储块归还到分区
